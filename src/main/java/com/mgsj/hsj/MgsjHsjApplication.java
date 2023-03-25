6b32c10b5f8f01a200667b61760428116dd4ef35
jiseob <sobysoby12@naver.com>
2023-03-25 15:23:55 +0900

    도커 예시 프로젝트 업로드

---
+++ src/main/java/com/mgsj/hsj/MgsjHsjApplication.java
@@ -0,0 +1,13 @@
+package com.mgsj.hsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgsjHsjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgsjHsjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgsj/hsj/MgsjHsjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgsj.hsj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgsjHsjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
